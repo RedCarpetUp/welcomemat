cc9528764d25d80652fb8c5f12d1e97ab58b99c0
SiDevesh <sid.swapnildevesh@gmail.com>
2017-05-16 18:35:52 +0530

    Add devise, remove api and dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
-    namespace :v1 do
-      resources :organisations, only: [:create, :show, :index, :update, :destroy] do
-        resources :jobs, only: [:create, :show, :index, :update, :destroy] do
-          resources :applications, only: [:show, :index, :create]
-        end
-      end
+  devise_for :users
+  #namespace :api, defaults: {format: :json} do
+  #  namespace :v1 do
+  #    resources :organisations, only: [:create, :show, :index, :update, :destroy] do
+  #      resources :jobs, only: [:create, :show, :index, :update, :destroy] do
+  #        resources :applications, only: [:show, :index, :create]
+  #      end
+  #    end
+  #  end
+  #end
+
+  #resources :organisations, only: [:show, :index] do
+  #  resources :jobs, only: [:show, :index] do
+  #    resources :applications, only: [:new, :create]
+  #  end
+  #  get 'dashboard', to: 'dashboard#index'
+  #  get 'dashboard/*other', to: 'dashboard#index'
+  #end
+  root to: "organisations#index"
+  resources :organisations do
+    resources :jobs, only: [:show, :index, :create, :new, :update, :edit, :destroy] do
+      resources :applications, only: [:show, :index, :create, :new]
     end
   end
-
-  resources :organisations, only: [:show, :index] do
-    resources :jobs, only: [:show, :index] do
-      resources :applications, only: [:new, :create]
-    end
-    get 'dashboard', to: 'dashboard#index'
-    get 'dashboard/*other', to: 'dashboard#index'
-  end
-
 
 end
